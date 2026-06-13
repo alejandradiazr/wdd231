df6ff6c6df54b8c2c730034fb86e6dc9ae4c0124
alejandradiazr <aleja.diaz.2023@gmail.com>
2026-06-12 22:24:30 -0500

    Chamber Discover Page

---
+++ chamber/data/places.mjs
@@ -0,0 +1,59 @@
+export const places = [
+
+{
+name: "Plaza de la Villa",
+address: "Downtown Sogamoso",
+description: "The historic central square of the city.",
+image: "images/plaza.webp"
+},
+
+{
+name: "Archaeological Museum",
+address: "Museum District",
+description: "Exhibits about Muisca culture.",
+image: "images/museum.webp"
+},
+
+{
+name: "Santa Barbara viewpoint",
+address: "Santa Barbara",
+description: "A viewpoint for the city.",
+image: "images/viewpoint.webp"
+},
+
+{
+name: "Plaza 6 de Septiembre",
+address: "Center Sogamoso",
+description: "Important place at the center of the region.",
+image: "images/plazas.webp"
+},
+
+{
+name: "Recreational Park",
+address: "Northern Sogamoso",
+description: "Recreational park with sports fields.",
+image: "images/park.webp"
+},
+
+{
+name: "El Laguito",
+address: "Northern Sogamoso",
+description: "Popular recreational area for families.",
+image: "images/laguito.webp"
+},
+
+{
+name: "Sogamoso Theater",
+address: "Sogamoso Center",
+description: "Historic theater of the city.",
+image: "images/theater.webp"
+},
+
+{
+name: "Pyramid Waterfall",
+address: "Southeast Sogamoso",
+description: "Green area and waterfalls.",
+image: "images/waterfall.webp"
+}
+
+];
